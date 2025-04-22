a3a2591b0235c058111907acaf4fe750d7fdff7a
LeoNS3771 <leonardonascimentosouza2020@gmail.com>
2025-04-22 15:30:09 -0300

    Update Main.java

--- lermoo/src/lermoo/Main.java
+++ lermoo/src/lermoo/Main.java
@@ -1,32 +1,47 @@
 package lermoo;
 import java.util.Scanner;
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.net.URL;
+import java.net.URI;
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
+    	List<String> listaPalavras = new ArrayList<>();
+    	List<String> listaPalavrasVer = new ArrayList<>();
     	
-        Scanner scanner = new Scanner(System.in);
-        Random gerador = new Random();
-        
-        // Gerando palavra aleatoria do arquivo
-        String palavra = null;
-        int posAleatoria = gerador.nextInt(100) + 1; // de 1 a 100
-        int i = 1;
-        
-        try (BufferedReader leitor = new BufferedReader(new FileReader("palavras.txt"))) {
-            while ((palavra = leitor.readLine()) != null) {
-                if (i == posAleatoria) {
-                    break;
-                }
-                i++;
+        try {
+        	URI uriC = URI.create("https://raw.githubusercontent.com/LeoNS3771/Lermo/refs/heads/main/palavrasLermo.txt");
+            URL palavrasCorretas = uriC.toURL();
+            
+            URI uriV = URI.create("https://gist.githubusercontent.com/vncsmnl/25e7c165da276405af8ca4e1c8e17806/raw/aaeb75a75ff48ae8cd8888bae031dcb9884cddaa/wordlist");
+            URL palavrasVerif = uriV.toURL();
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(palavrasCorretas.openStream()));
+            BufferedReader in2 = new BufferedReader(new InputStreamReader(palavrasVerif.openStream()));
+            
+            String linha;
+            while((linha = in.readLine()) != null) {
+            	listaPalavras.add(linha.trim());
             }
-        } catch (IOException e) {
-            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+            while((linha = in2.readLine()) != null) {
+            	listaPalavrasVer.add(linha.trim());
+            }
+            
+            in.close();
+            in2.close();
+            
+        } catch(Exception e){
+        	System.out.println("Erro ao acessar a lista de palavras: " + e.getMessage());
         }
-        	
+        
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String palavraEscolhida = listaPalavras.get(random.nextInt(listaPalavras.size()));
+        
         // Definindo variaveis
         Lermo lermo = new Lermo();
         String entrada;
@@ -43,18 +58,18 @@ public class Main {
             // Entrada do jogador
             do {
             	System.out.print("-> ");
-                entrada = scanner.nextLine().toLowerCase();
+                entrada = scanner.nextLine().toUpperCase();
                 
-            } while (!lermo.validarPalavra(entrada));
+            } while (!lermo.validarPalavra(entrada, listaPalavrasVer));
 
-            if (lermo.compararPalavras(palavra, entrada)) {
+            if (lermo.compararPalavras(palavraEscolhida, entrada)) {
                 System.out.println("\nVoce acertou!\nCom " + vidas + " vidas restantes");
                 scanner.close();
                 return;
             }
             vidas--;
         }
-        System.out.println("Voce perdeu\n" + "A palavra era: " + palavra);
+        System.out.println("Voce perdeu\n" + "A palavra era: " + palavraEscolhida);
         
         scanner.close();
     }
